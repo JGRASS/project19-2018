9c83e17bb84f7091e8b31b4d86c1fa9c22abf861
Sofija Dimitrijevic <dimitrijevic.sofijaa@gmail.com>
2018-05-08 15:47:44 +0200

    Dokumentovane klase SOIstorija i SOZakazaniPregledi.

--- DomZdravlja/src/sistemskeoperacije/SOIstorija.java
+++ DomZdravlja/src/sistemskeoperacije/SOIstorija.java
@@ -3,6 +3,10 @@ package sistemskeoperacije;
 import domZdravlja.klase.Pacijent;
 
 public class SOIstorija {
+	/**
+	 * Varca String koji predstavlja istoriju pregleda.
+	 * @return String koji predstavlja istoriju pregleda
+	 */
 	public static String izvrsi() {
 		String s = "";
 		for (int i = 0; i < Pacijent.istorijaPregleda.size(); i++) {
--- DomZdravlja/src/sistemskeoperacije/SOZakazaniPregledi.java
+++ DomZdravlja/src/sistemskeoperacije/SOZakazaniPregledi.java
@@ -3,6 +3,10 @@ package sistemskeoperacije;
 import domZdravlja.klase.Pacijent;
 
 public class SOZakazaniPregledi {
+	/**
+	 * Varca String koji predstavlja zakazane preglede.
+	 * @return String koji predstavlja zakazane preglede
+	 */
 	public static String izvrsi() {
 		String s = "";
 		for (int i = 0; i < Pacijent.zakazaniPregledi.size(); i++) {
