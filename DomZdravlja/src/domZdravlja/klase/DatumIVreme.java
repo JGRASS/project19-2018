07c87f36acb79abd6aaf789ec72c3e44d2e898c6
Sofija Dimitrijevic <dimitrijevic.sofijaa@gmail.com>
2018-05-08 15:41:19 +0200

    Uveden SimpleDateFormat u klasu DatumIVreme. Izmenjena toString metoda.

--- DomZdravlja/src/domZdravlja/klase/DatumIVreme.java
+++ DomZdravlja/src/domZdravlja/klase/DatumIVreme.java
@@ -1,6 +1,8 @@
 package domZdravlja.klase;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
 /**
  * Klasa predstavlja pacijenta sa osnovnim osobinama.
  * 
@@ -9,9 +11,9 @@ import java.util.Date;
  */
 
 public class DatumIVreme {
-    /**
-     * Predstavlja datum. Pripada klasi java.util.Date.
-     */
+	/**
+	 * Predstavlja datum. Pripada klasi java.util.Date.
+	 */
 	private Date datum;
 	/**
 	 * Predstavlja sate.
@@ -22,57 +24,78 @@ public class DatumIVreme {
 	 */
 	private int minuti;
 	/**
+	 * Format koji ce se koristiti za prikazivanje datuma.
+	 */
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	/**
 	 * Vraca datum.
+	 * 
 	 * @return datum
 	 */
 	public Date getDatum() {
 		return datum;
 	}
+
 	/**
 	 * Postavlja datum ukoliko uneti parametar nije <b>null</b>.
+	 * 
 	 * @param datum
-	 * @throws RuntimeException ukoliko je uneti parametar null
+	 * @throws RuntimeException
+	 *             ukoliko je uneti parametar null
 	 */
 	public void setDatum(Date datum) {
-		if(datum!=null) this.datum = datum;
-		else throw new RuntimeException("Greska, datum ne moze imati vrednost null.");
+		if (datum != null)
+			this.datum = datum;
+		else
+			throw new RuntimeException("Greska, datum ne moze imati vrednost null.");
 	}
+
 	/**
 	 * Vraca sate.
+	 * 
 	 * @return sati
 	 */
 	public int getSati() {
 		return sati;
 	}
+
 	/**
 	 * Postavlja vrednos sati.
+	 * 
 	 * @param sati
 	 */
 	public void setSati(int sati) {
 		this.sati = sati;
 	}
+
 	/**
 	 * Vraca minute.
+	 * 
 	 * @return minuti
 	 */
 	public int getMinuti() {
 		return minuti;
 	}
+
 	/**
 	 * Postavlja vrednost minuta.
+	 * 
 	 * @param minuti
 	 */
 	public void setMinuti(int minuti) {
 		this.minuti = minuti;
 	}
+
 	/**
 	 * Vraca String koji prikazuje vrednosti atributa klase datumIVreme.
+	 * 
 	 * @return String koji prikazuje vrednosti atributa klase datumIVreme
 	 */
 	@Override
 	public String toString() {
-		return "Datum: " + datum + "  Sati: " + sati + "  Minuti: " + minuti;
+		String sDate = sdf.format(datum);
+		return "Datum: " + sDate + "  Sati: " + sati + "  Minuti: " + minuti;
 	}
-	
-	
+
 }
